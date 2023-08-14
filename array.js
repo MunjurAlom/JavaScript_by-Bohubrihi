61bf035851210f7ca4e120bbcef32e6463381ad5
Munjur Alom <97244984+MunjurAlom@users.noreply.github.com>
2023-08-14 23:05:19 +0600

    Update array.js

--- array.js
+++ array.js
@@ -1,4 +1,26 @@
-var countries = ["Bangladesh", "USA", "UK"];
+//Collection of items
+countries = ["Bangladesh", "USA", "UK"]
+
+countries[0]
+countries[1]
+
+country = countries[0]
+country
+
+//Mutable
+countries[0] = "Poland"
+countries
+countries.length;
+countries[countries.length] = "Norway"
+
+//Push and Pop
+countries.push("China"); // return length
+countries.pop() // returns length
+countries.push("Japan", "Srilanka");
+countries.shift() //returns the shifted
+countries.unshift("Germany") // returns length
+
+*/ var countries = ["Bangladesh", "USA", "UK"];
 document.write(countries);
 var country = countries[1];
 document.write("<br>",countries);
@@ -8,8 +30,9 @@ countries[1] = "Proland"; // change country name in the position of index 1
 country =countries[1];
 document.write("<br>",country);
 document.write("<br>",countries); // view countries after changing
-// now insert new country in this array
+//Now insert a new country in this array
 countries[3] = "Norway";
 document.write("<br>",countries); 
 countries[countries.length] = "Sweden";
 document.write("<br>",countries); 
+*/
